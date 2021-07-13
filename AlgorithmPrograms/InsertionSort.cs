1fa74e0152e1c25480112787b4a844c4931a2187
ahamedsahib <adsahib39@gmail.com>
2021-07-14 01:53:58 +0530

    [Ahamed]Performing Insertion Sort to sort the array

---
+++ AlgorithmPrograms/InsertionSort.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace AlgorithmPrograms
+{
+    public class InsertionSort
+    {
+        public void InputArray()
+        {
+
+            int[] arr = { 12,1,26,4,3,13, 5, 6 };
+            Console.WriteLine("Array Before Sorting");
+            PrintArray(arr);
+            sort(arr);
+            Console.WriteLine("Array After sorted");
+            PrintArray(arr);
+
+        }
+        void sort(int[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int key = arr[i];
+                int j = i - 1;
+
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+                arr[j + 1] = key;
+            }
+        }
+         public void PrintArray(int[] array)
+         {
+            foreach(int i in array)
+            {
+                Console.WriteLine(i);
+            }
+         }
+        
+    }
+}
